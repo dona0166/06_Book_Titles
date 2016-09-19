bd27fe00966df10a1f2159eb64d09a59b0f7ce81
Donnie <Donnie@DESKTOP-A0TIJCR>
2016-09-19 09:38:07 +0200

    Add project files.

---
+++ 06_Book_Titles/Book.cs
@@ -0,0 +1,30 @@
+﻿namespace _06_Book_Titles
+{
+    public class Book
+    {
+        public string Title(string something)
+        {
+            string result = "";
+            string[] anarray = something.Split(' ');
+            for(int i = 0; i<anarray.Length; i++)
+            {
+                if(anarray[i] == "the" || anarray[i] == "a" || anarray[i] == "an" || anarray[i] == "and")
+                {
+                    if(i == 0)
+                    {
+                        result += anarray[i].Substring(0, 1).ToUpper() + anarray[i].Substring(1) + " ";
+                    }
+                    else
+                    {
+                        result += anarray[i] + " ";
+                    }
+                }
+                else
+                {
+                    result += anarray[i].Substring(0, 1).ToUpper() + anarray[i].Substring(1) + " ";
+                }
+            }
+            return result.Trim();
+        }
+    }
+}
